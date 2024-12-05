ce2f4c8906ef8477c98dcd7e246102ac4bfe078a
Mohammad <=>
2024-12-05 23:13:26 +0330

    Add test pathing

--- api/cmd/serve.go
+++ api/cmd/serve.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -13,7 +14,16 @@ var serveCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Mysql DSN", os.Getenv("MYSQL_DSN"))
 
-		fmt.Println(echo.New().Start(":8080"))
+		e := echo.New()
+
+		e.Use(middleware.Logger())
+		e.Use(middleware.Recover())
+
+		e.GET("/ok", func(c echo.Context) error {
+			return c.String(200, "ok")
+		})
+
+		e.Logger.Error(e.Start(":8080"))
 	},
 }
 
